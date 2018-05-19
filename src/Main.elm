28f68947709afcd69bafb9584b37f5fa3dd89816
Maja Kudlicka <maja.kudlicka@gmail.com>
2018-05-19 11:35:58 +0100

    feat - fetched resources from back end

--- src/Main.elm
+++ src/Main.elm
@@ -1,51 +1,16 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
-
-
--- MODEL
-
-
-type alias Model =
-    String
+import Html exposing (program)
+import Msgs exposing (Msg)
+import Models exposing (Model, initialModel)
+import Update exposing (update)
+import View exposing (view)
+import Commands exposing (fetchPlayers)
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Hello", Cmd.none )
-
-
-
--- MESSAGES
-
-
-type Msg
-    = NoOp
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
+    ( initialModel, fetchPlayers )
 
 
 subscriptions : Model -> Sub Msg
